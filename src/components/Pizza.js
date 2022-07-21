b0ea668daeb6487673e20d5ec294812ca1b44c67
aryan-dubey <itsaryanborn2rock@gmail.com>
2022-07-22 02:20:13 +0530

    PizzaMgmt FE Updated

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,10 +1,21 @@
 import React, {useState, useEffect} from 'react'
 // import axios from 'axios';
-import { NavLink } from "react-router-dom";
+//import { useHistory } from "react-router-dom";
 import './pizza.css';
+import Popup from 'reactjs-popup';
+
 
 const Pizza = () => {
     const [pizzas, setPizzas] = useState([]);
+    const [addPizza, setAddPizza] = useState({
+      pizzaType: "",
+      pizzaName: "",
+      pizzaDescription: "",
+      pizzaCost: ""
+    });
+
+
+
 
     const viewPizza = async (e) => {
       const res = await fetch(`http://localhost:8080/viewPizza`, {
@@ -13,7 +24,7 @@ const Pizza = () => {
           "Content-Type": "application/json",
         }
       });
-  
+    
       const data = await res.json();
       console.log(data);
   
@@ -23,20 +34,85 @@ const Pizza = () => {
       } else {
         setPizzas(data);
         // window.alert("Data Fetch Successfully");
-        console.log("1" + pizzas)
+        //console.log("1" + pizzas)
       }
+    }
       
      
+      const setPizzaDetails = async (e) => {
+        const{name,value}=e.target;
+        setAddPizza((preData)=>{
+          return{
+              ...preData,
+              [name]:value
+          }
+      })
+      }
 
-    };
+      const addPizzaDetails = async (e) => {
+        e.preventDefault();
+      
+        
+        const{pizzaType, pizzaName, pizzaDescription , pizzaCost} = addPizza;
+
+        const res = await fetch('http://localhost:8080/addPizza/1',{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                pizzaType, pizzaName, pizzaDescription , pizzaCost
+            })
+        });
+
+        const addData = await res.json();
+
+        if(res.status === 404 || !addData){
+            window.alert("Error: Data Not Save");
+            console.log("Data Not Saved");
+            
+        }else{
+            window.alert("Data Save Successfully");
+        }
+         
+      
+      
+      
+      };
   
+    
     useEffect(()=>{
       viewPizza()
-    },[])
-  
+        },[])
+        
+
   return (
     <>
-   <h1 className="text-center">View Pizza</h1>
+   <h1 className="text-center">Pizza Management</h1>
+                              <Popup trigger={<button className="btn btn-primary mx-5 pizzabtn ">Add Pizza</button>} position="right center">
+                                 <div className="popup">
+                                <h2>Add Pizza</h2>
+                                <form>
+                                  <div className="form-group">
+                                    <label htmlFor="pizzaType">Pizza Type</label>
+                                    <input type="text" className="form-control" id="pizzaType" placeholder="Enter Pizza Type" onChange={setPizzaDetails} value={addPizza.pizzaType} name="pizzaType"/>
+                                  </div>
+                                  <div className="form-group">
+                                    <label htmlFor="pizzaName">Pizza Name</label>
+                                    <input type="text" className="form-control" id="pizzaName" placeholder="Enter Pizza Name" onChange={setPizzaDetails} value={addPizza.pizzaName} name="pizzaName" />
+                                  </div>
+                                  <div className="form-group">
+                                    <label htmlFor="pizzaDescription">Pizza Description</label>
+                                    <input type="text" className="form-control" id="pizzaDescription" placeholder="Enter Pizza Description" onChange={setPizzaDetails} value={addPizza.pizzaDescription} name={"pizzaDescription"} />
+                                  </div>
+                                  <div className="form-group">
+                                    <label htmlFor="pizzaCost">Pizza Cost</label>
+                                    <input type="text" className="form-control" id="pizzaCost" placeholder="Enter Pizza Cost" onChange={setPizzaDetails} value={addPizza.pizzaCost} name={"pizzaCost"} />
+                                  </div>
+                                  <button type="submit" className="btn btn-primary" onClick={addPizzaDetails}>Submit</button>
+                                </form>
+                              </div>
+                            </Popup>
     <div className="containers">
         <table className="table table-striped">
             <thead>
@@ -62,22 +138,59 @@ const Pizza = () => {
                           <td>{val.pizzaDescription}</td>
                           <td>{val.pizzaCost}</td>
                           <td>
-                          <NavLink to="" className="btn btn-primary">Add Pizza</NavLink> &nbsp;
-                          <NavLink to="" className="btn btn-info">Update Pizza</NavLink> &nbsp;
-                          <NavLink to="" className="btn btn-danger">Delete Pizza</NavLink>
-                            </td>
-                          </tr>
+                            
+                            <Popup trigger={<button className="btn btn-primary">Update Pizza</button>} position="right center">
+                              <div className="popup">
+                                <h2>Update Pizza</h2>
+                                <form>
+                                  <div>
+                                  <label htmlFor="pizzaType">Pizza id</label>
+                                    <input type="text" className="form-control" id="pizzaId" placeholder="Enter Pizza id" />
+                                  </div>
+                                  <div className="form-group">
+                                    <label htmlFor="pizzaType">Pizza Type</label>
+                                    <input type="text" className="form-control" id="pizzaType" placeholder="Enter Pizza Type" />
+                                  </div>
+                                  <div className="form-group">
+                                    <label htmlFor="pizzaName">Pizza Name</label>
+                                    <input type="text" className="form-control" id="pizzaName" placeholder="Enter Pizza Name" />
+                                  </div>
+                                  <div className="form-group">
+                                    <label htmlFor="pizzaDescription">Pizza Description</label>
+                                    <input type="text" className="form-control" id="pizzaDescription" placeholder="Enter Pizza Description" />
+                                  </div>
+                                  <div className="form-group">
+                                    <label htmlFor="pizzaCost">Pizza Cost</label>
+                                    <input type="text" className="form-control" id="pizzaCost" placeholder="Enter Pizza Cost" />
+                                  </div>
+                                  <button type="submit" className="btn btn-primary">Submit</button>
+                                </form>
+                              </div>
+                            </Popup>
+                            <Popup trigger={<button className="btn btn-primary">Delete Pizza</button>} position="right center">
+                              <div className="popup">
+                                <h2>Delete Pizza</h2>
+                                <form>
+                                  <div className="form-group">
+                                    <label htmlFor="pizzaType">Pizza id</label>
+                                    <input type="text" className="form-control" id="pizzaId" placeholder="Enter Pizza id" />
+                                  </div>
+                                  <button type="submit" className="btn btn-primary">Submit</button>
+                                </form>
+                              </div>
+                            </Popup>
+                          </td>
+                        </tr>
                     </>
                   )
-                })
-               }
-              
+                }
+                )
+              }
             </tbody>
           </table>
     </div>
-
-    </>
-)
+    </>
+  );
 }
 
-export default Pizza;
+export default Pizza;
